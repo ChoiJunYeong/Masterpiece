961e0aa29c513adce2b3792c351998f3789b5ead
soungonkim <soungonkim@kaist.ac.kr>
2018-08-01 15:49:31 +0900

    Merge branch 'master' of https://github.com/ChoiJunYeong/Masterpiece
    # Conflicts:
    #	app/src/main/java/com/example/masterpiece/masterpiece/ResultActivity.java

--- app/src/main/java/com/example/masterpiece/masterpiece/ResultActivity.java
+++ app/src/main/java/com/example/masterpiece/masterpiece/ResultActivity.java
@@ -21,6 +21,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import java.io.ByteArrayOutputStream;
